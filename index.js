6694d01835e713febabec7dcf74b2c1be9af0aa1
John Wells <molekewl2017@gmail.com>
2019-11-04 10:38:19 -0800

    logger and connect files

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const server = express();
 
+const projectModel = require('./router/projectsRouter');
+
+// Custom Logger Middleware
+const logger = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.path} from ${req.get('host')}`);
+  next();
+}
+
 server.use(express.json());
+server.use(logger);
+server.use('/api/projects', projectModel);
 
 server.get('/', (req, res) => {
-  res.send('Server is connected')
-})
+  res.send('Server is connected');
+});
 
 const port = 8000
 
